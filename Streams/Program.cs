8fc15014e149b06e7f5a4757dca98f7ef6e32f73
SaprankovaLE <saprankovale@mer.ci.nsu.ru>
2024-03-11 14:36:50 +0700

    Добавила первую лабораторную

---
+++ Streams/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Streams
+{
+    class Program
+    {
+        public delegate void ThreadStart();
+        static void Main()
+        {
+            Thread Tread1 = new Thread(Robot2Move); //создание второго потока
+            Tread1.Start();
+            Robot1Move();
+        }
+        static void Robot1Move() //выполняется в главном потоке
+        {
+            Robot robot1 = new Robot(0, 0, "Робот 1", "Вперед");
+            for (int i = 0; i < 10; i++)
+            {
+                robot1.Move();
+                Thread.Sleep(100);
+            }
+        }
+        static void Robot2Move() //выполняется во втором потоке
+        {
+            Robot robot2 = new Robot(0, 0, "Робот 2", "Вперед");
+            for (int i = 0; i < 10; i++)
+            {
+                robot2.Move();
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ Streams/Robot.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+    public class Robot
+    {
+        private Tuple<int, int> coordinates;
+        private string name;
+        private string direction { get; set; }
+
+        public Robot(int x, int y, string name, string direction) //конструктор класса робот
+        {
+            coordinates = new Tuple<int, int>(x, y);
+            this.name = name;
+            this.direction = direction;
+        }
+
+        public void Move()
+        {
+            Random rand = new Random();
+            int movement = rand.Next(0, 4);
+
+            switch (movement)
+            {                
+                case 0:
+                    coordinates = new Tuple<int, int>(coordinates.Item1, coordinates.Item2 + 1); //Вперед
+                    direction = "Вперед";
+                    Console.WriteLine(GetInfo());
+                    break;
+                case 1:
+                    coordinates = new Tuple<int, int>(coordinates.Item1, coordinates.Item2 - 1); //Назад
+                    direction = "Назад";
+                    Console.WriteLine(GetInfo());
+                    break;
+                case 2:
+                    coordinates = new Tuple<int, int>(coordinates.Item1 + 1, coordinates.Item2); //Направо
+                    direction = "Направо";
+                    Console.WriteLine(GetInfo());
+                    break;
+                case 3:
+                    coordinates = new Tuple<int, int>(coordinates.Item1 - 1, coordinates.Item2); //Налево
+                    direction = "Налево";
+                    Console.WriteLine(GetInfo());
+                    break;
+                default:
+                    break;
+            }
+        }
+        public string GetInfo()
+        {
+            return $"{name} {direction} ({coordinates.Item1}, {coordinates.Item2})";
+        }
+    }
